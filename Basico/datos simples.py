88cba0853628d8d71dbcf8bf30c7ab5594746275
Mateo Congo <aidenperse@gmail.com>
2024-02-11 09:21:14 -0500

    Inicios

---
+++ Basico/datos simples.py
@@ -0,0 +1,23 @@
+"string"
+'string'
+
+"""
+    nombre: Mateo
+    apellido: Congo
+"""
+
+'''
+    edad: 22
+    estatura: 1.80
+'''
+
+# Entero (int)
+4
+
+# Float
+5.6
+
+# Booleanos
+True
+
+False
---
+++ Basico/variables.py
@@ -0,0 +1,24 @@
+# Definiendo variables
+a = 4
+b = 5
+c = a + b
+
+print("El valor de a + b es: ", c)
+
+# Concatenación
+
+# Concatenación con +
+saludo = "Hola " + "¿Cómo estas?"
+
+print(saludo)
+
+# Concatenación con f-strings
+nombre = "Juan"
+edad = 20
+presentacion = f"Mi nombre es {nombre}, y tengo {edad} años"
+
+print(presentacion)
+# Operadores (in / not in)
+
+print("Juan" in presentacion) # True
+print("Juan" not in presentacion) # False
